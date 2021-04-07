96075e06588e11cd4127c55414cb39aefb84a8b1
Howakyl96 <howakyl@gmail.com>
2021-04-07 14:19:14 -0700

    grapqhl api running

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql')
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server running on PORT:${PORT}.`));
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloWorld',
+    fields: () => ({
+      message: { 
+        type: GraphQLString, 
+        resolve: () => 'HelloWorld'
+      }
+    })
+  })
+})
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema: schema
+}))
+app.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
+
