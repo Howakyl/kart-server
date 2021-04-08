6a3ecd0dc4da873426208c5784713f9c8968de05
Howakyl96 <howakyl@gmail.com>
2021-04-07 17:27:37 -0700

    commented out unnecessary gql code for now

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString
-} = require('graphql');
+// const {graphqlHTTP} = require('express-graphql');
+// const {
+//   GraphQLSchema,
+//   GraphQLObjectType,
+//   GraphQLString
+// } = require('graphql');
 const routes = require('./routes');
 const app = express();
 const PORT = 5000;
@@ -13,24 +13,24 @@ const PORT = 5000;
 const router = require('express').Router();
 
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'HelloWorld',
-    fields: () => ({
-      message: { 
-        type: GraphQLString, 
-        resolve: () => 'HelloWorld'
-      }
-    })
-  })
-})
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: 'HelloWorld',
+//     fields: () => ({
+//       message: { 
+//         type: GraphQLString, 
+//         resolve: () => 'HelloWorld'
+//       }
+//     })
+//   })
+// })
 
 
 app.use('/items', routes.items);
 
-app.use('/graphql', graphqlHTTP({
-  graphiql: true,
-  schema: schema
-}))
+// app.use('/graphql', graphqlHTTP({
+//   graphiql: true,
+//   schema: schema
+// }))
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
 
