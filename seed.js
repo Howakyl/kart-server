eb6023ace2bfc30ae1675a67d1bde040a0556edd
Howakyl96 <howakyl@gmail.com>
2021-04-16 15:01:33 -0700

    seed file used to create item data, seed command in package.json. files successfully seeded

--- seed.js
+++ seed.js
@@ -1,14 +1,131 @@
-const db = require('./src/models');
-const data = require('./items.json');
+import { Item } from './src/models/Item';
+import mongoose from 'mongoose';
 
-db.Item.deleteMany({}, (err, deletedItems) => {
-  if (err) console.log(err);
-  console.log(`DELETED ITEMS: ${deletedItems}`)
-  db.Item.create(data, (err, seededItems) => {
-    if (err) console.log(err);
+mongoose.connect('mongodb://localhost:27017/shopping-kart', 
+    { useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  );
 
-    console.log(data, "items created successfully!");
+const data = [
+  "cherimoya",
+  "cherry",
+  "pineapple",
+  "pinapple",
+  "pine nuts",
+  "almonds",
+  "milk",
+  "almond milk",
+  "broccoli",
+  "broccolini",
+  "pie",
+  "cherry pie",
+  "blueberry pie",
+  "blueberries",
+  "apples",
+  "apple pie",
+  "avocado",
+  "sugar",
+  "bacon",
+  "eggs",
+  "tofu",
+  "tofurkey",
+  "potatoes",
+  "patatoes",
+  "bananas",
+  "grapes",
+  "grapefruit",
+  "grapefruit juice",
+  "grape juice",
+  "orange juice",
+  "oranges",
+  "orange bandana",
+  "orangutan stuffed animal",
+  "oranigram games and puzzled",
+  "diapers",
+  "tea tree shampoo",
+  "hand soap",
+  "dishsoap",
+  "hand sanitizer",
+  "paper plates",
+  "paper towels",
+  "raspberries",
+  "cat food",
+  "tuna fish",
+  "kibbles",
+  "English breakfast tea",
+  "green tea",
+  "cherry chapstick",
+  "sirloin steak",
+  "New York strip steak",
+  "chicken nuggets",
+  "sparkling water",
+  "coconut water",
+  "coconut flakes",
+  "taco mix",
+  "trailmix",
+  "spam",
+  "beef broth",
+  "chicken soup",
+  "tomato soup",
+  "cherry tomatoes",
+  "beefsteak tomatoes",
+  "sun-dried tomatoes",
+  "sunscreen",
+  "sunblock",
+  "shaving cream",
+  "whipped cream",
+  "creamy tomato soup",
+  "chocolate candy bar",
+  "chocolate-covered cherries",
+  "milk",
+  "chocolate milk",
+  "protein bar",
+  "bar soap",
+  "pickles",
+  "pickled fish",
+  "olive oil",
+  "olives",
+  "coconut oil",
+  "avocado oil",
+  "zucchini",
+  "zuchini",
+  "peanut butter",
+  "almond butter",
+  "peanuts",
+  "macadamia nuts",
+  "butter"
+  ]
 
-    process.exit();
-  });
-});
+  function makeItem(item) {
+    const newItem = new Item(item);
+    newItem.save((err, result) => {
+      if (err) console.log(err)
+    })
+    console.log(newItem)
+  }
+
+  function exit() {
+    mongoose.disconnect();
+  }
+
+  function seedDb () {
+    let done = 0;
+
+    Item.deleteMany({}, (err, result) => {
+      if (err) console.log(err)
+    })
+
+    for (let i = 0; i < data.length; i++) {
+      const itemObj = {name: data[i]}
+      makeItem(itemObj)
+      done++;
+      if (done === data.length) {
+        // exit();
+      }
+    }
+  }
+
+  seedDb();
