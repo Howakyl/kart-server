2c13425b8e96b4cdf86ecce61647c5553c9ca32b
Howakyl96 <howakyl@gmail.com>
2021-04-18 13:14:34 -0700

    createItem mutation in itemResolver

--- src/resolvers/itemResolver.js
+++ src/resolvers/itemResolver.js
@@ -3,5 +3,13 @@ import { Item } from '../models/Item';
 export const itemResolver = {
   Query: {
     hello: () => 'hello, world!'
+  },
+
+  Mutation: {
+    createItem: async (_, {name}) => {
+      const newItem = new Item({name});
+      await newItem.save();
+      return newItem;
+    },
   }
 }
